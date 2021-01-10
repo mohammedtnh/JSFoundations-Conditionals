1b4e0887f7cca82c99aee4af15a6c84798d56831
Mohammed Hamdan <mohammed.tnh@gmail.com>
2021-01-10 15:37:25 +0300

    Done

--- calculator.js
+++ calculator.js
@@ -1,3 +1,33 @@
 // Define the numbers and the operation here
+const firstNumber = 3;
+const secondNumber = 5;
+const operation = "+";
 
 // Write if-statements here
+if (operation === "+") {
+  console.log(
+    `${firstNumber} ${operation} ${secondNumber} = ${
+      firstNumber + secondNumber
+    }`
+  );
+} else if (operation === "-") {
+  console.log(
+    `${firstNumber} ${operation} ${secondNumber} = ${
+      firstNumber - secondNumber
+    }`
+  );
+} else if (operation === "*") {
+  console.log(
+    `${firstNumber} ${operation} ${secondNumber} = ${
+      firstNumber * secondNumber
+    }`
+  );
+} else if (operation === "/") {
+  console.log(
+    `${firstNumber} ${operation} ${secondNumber} = ${
+      firstNumber / secondNumber
+    }`
+  );
+} else {
+  console.log("This is a calculator, choose + or - or * or /");
+}
